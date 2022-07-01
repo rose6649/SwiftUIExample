9aeae26a1bd5f8ae593b5b4495c72209d8c1de90
Hiju <rose6649@likelion.org>
2022-07-01 14:11:30 +0900

    add: 설정초기화

---
+++ SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by hiju on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
